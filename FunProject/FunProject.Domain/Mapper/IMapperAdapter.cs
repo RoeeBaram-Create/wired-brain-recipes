d9e0a41d7f8cacde0d4e58042d92f3a899e08c34
Roee Baram <roeebaram@gmail.com>
2020-11-08 09:06:18 +0200

    asdsds

--- FunProject/FunProject.Domain/Mapper/IMapperAdapter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FunProject.Domain.Mapper
-{
-    public interface IMapperAdapter
-    {
-        TDestination Map<TDestination>(object source);
-        TDestination Map<TSource, TDestination>(TSource source);
-        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
-    }
-}
